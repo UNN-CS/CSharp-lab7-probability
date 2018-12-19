4bbb13fbf31feb2acfec014da76b5d2d2ceff508
aYellowa <golo400@icloud.com>
2018-12-19 22:20:06 +0300

    7

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab77
+{
+    class Program
+    {
+        Stopwatch watch = new Stopwatch();
+        private static bool Sort(int[] basa)
+        {
+            int Long = basa.Length;
+
+            while (--Long >= 1)
+                if (basa[Long] < basa[Long - 1])
+                    return false;
+            return true;
+
+        }
+
+        private static void Rand(int[] stan)
+        {
+            int tmp, rnd;
+            Random rand = new Random();
+            for (int i = 0; i < stan.Length; i++)
+            {
+                rnd = rand.Next(stan.Length);
+                tmp = stan[i];
+                stan[i] = stan[rnd];
+                stan[rnd] = tmp;
+            }
+        }
+        static void Form1(string[] args)
+        {
+
+
+
+            int N = 0;
+            N++;
+
+            while (N != 0)
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                Console.Write("шаг" + N);
+                Console.WriteLine("");
+                int[] data = new int[N];
+                Random rd = new Random();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = rd.Next(1, 101);
+                    Console.Write(" ");
+                    Console.Write(data[i]);
+                }
+                Console.WriteLine("");
+                VpolneniiBoogSort(data);
+                for (int i = 0; i < data.Length; i++)
+                {
+                    Console.Write(" ");
+                    Console.Write(data[i]);
+                }
+                watch.Stop();
+                Console.WriteLine("");
+                Console.Write("Время выполнения программы в часах" + "   " + watch.Elapsed.Hours + "   " + "час.\r\n");
+                Console.Write("Время выполнения программы в минутах" + "   " + watch.Elapsed.Minutes + "   " + "мин.\r\n");
+                Console.Write("Время выполнения программы в секундах" + "   " + watch.Elapsed.Seconds + "   " + "сек.\r\n");
+                Console.Write("Время выполнения программы в миллисекундах" + "   " + watch.ElapsedMilliseconds + "   " + "милсек.\r\n");
+                N++;
+            }
+        }
+        public static void VpolneniiBoogSort(int[] data)
+        {
+            while (!Sort(data))
+                Rand(data);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = 5;
+            int x = 0;
+            int[] stan = new int[N];
+            int[] Q = new int[N];
+            Random rd = new Random();
+            for (int i = 0; i < stan.Length; i++)
+            {
+                Q[i] = -1;
+            }
+            for (int i = 0; i < stan.Length; i++)
+            {
+                stan[i] = rd.Next(1, 5);
+            }
+            for (int i = 0; i < stan.Length; i++)
+            {
+                for (int j = 0; j < stan.Length; j++)
+                {
+
+                    if (stan[i] == Q[j])
+                    {
+                        break;
+                    }
+                    if (stan[i] == stan[j])
+                    {
+                        ++x;
+                    }
+                }
+                Q[i] = stan[i];
+                chart1.Series[0].Points.AddXY(stan[i], x);
+                x = 0;
+            }
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
