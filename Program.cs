7ea6686f0b0f0f39a3af452d95cc6878c5efffc8
Velnovikova <45849314+Velnovikova@users.noreply.github.com>
2018-12-27 05:47:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Probability
+{
+    class RandArray
+    { 
+        static Random rnd = new Random();
+        static public int[] RandFill(int N, int M)
+        {
+            
+            int[] mass = new int[N];
+            for (int i = 0; i < N; i++)
+                mass[i] = rnd.Next(0, M);
+            return mass;
+        }
+        static public int[] Count(int[] mass, int M)
+        {
+            int[] num = new int[M];
+            for (int i = 0; i < mass.Length; i++)
+                num[mass[i]]++;
+            return num;
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
